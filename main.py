67a0d5caa4c20a083b2ebaa5c4398c0e2697a041
Abdullah Qasim <127170762+abdullahqasim01@users.noreply.github.com>
2025-02-22 22:36:30 +0500

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://aq225.atwebpages.com"])
 
 @app.route("/tracks", methods=["GET"])
 def get_tracks():
